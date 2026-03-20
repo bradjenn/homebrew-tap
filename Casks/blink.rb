fe749fcc5f0b55a45203e6b96d1ee0f78e687465
Bradley Jennings <bjennings89@gmail.com>
2026-03-20 09:19:58 +0000

    Add blink cask formula

---
+++ Casks/blink.rb
@@ -0,0 +1,18 @@
+cask "blink" do
+  version "0.1.0"
+  sha256 "PLACEHOLDER"
+
+  url "https://github.com/bradjenn/blink/releases/download/v#{version}/Blink.dmg"
+  name "Blink"
+  desc "Terminal workspace with column-based tiling"
+  homepage "https://github.com/bradjenn/blink"
+
+  depends_on macos: ">= :sonoma"
+
+  app "Blink.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.blink.app.plist",
+    "~/Library/Application Support/com.blink.app",
+  ]
+end
