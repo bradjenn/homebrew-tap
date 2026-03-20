8ecd46e92286aba1050e8b6d967a40b50c09d953
Bradley Jennings <bjennings89@gmail.com>
2026-03-20 10:14:28 +0000

    Update sha256 for rebuilt v0.1.0

--- Casks/blink.rb
+++ Casks/blink.rb
@@ -1,6 +1,6 @@
 cask "blink" do
   version "0.1.0"
-  sha256 "87b4687ce3737c20158d725243fdf5dfeb5fe3d6d8ed9259b5ba8fc2aaab0b4f"
+  sha256 "6e4217297d05a90dff14153a4520c1210f6f42198e241d056836acdd4f1bdf0b"
 
   url "https://github.com/bradjenn/blink/releases/download/v#{version}/Blink.dmg"
   name "Blink"
